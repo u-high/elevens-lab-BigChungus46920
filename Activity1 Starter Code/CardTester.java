75f975405e3d85b73a4ebfcf6eb8a7bcd5f9f4b4
william Duke <wduke2002@gmail.com>
2019-02-13 14:54:58 -0600

    day 1
    activities 1 and start of 2

--- Activity1 Starter Code/CardTester.java
+++ Activity1 Starter Code/CardTester.java
@@ -3,11 +3,13 @@
  */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-	}
+ /**
+  * The main method in this class checks the Card operations for consistency.
+  * @param args is not used.
+  */
+ public static void main(String[] args) {
+  /* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+   Card dog = new Card("dog","spades", 10);
+   System.out.println(dog);
+ }
 }
